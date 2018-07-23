f39bd27e0bfe2c59402ec1c48d5b591c25267892
Kris Lee <krislee@Kriss-MacBook-Pro.local>
2018-07-23 20:24:20 +0900

    adding realm db

--- customCalendar2/cellinfo.swift
+++ customCalendar2/cellinfo.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+import RealmSwift
+
+class cellinfo: Object {
+    @objc dynamic var date: String = ""
+    @objc dynamic var filepath: String = ""
+    @objc dynamic var holiday: String = ""
+    
+    
+    //    override static func primaryKey() -> String? {
+    //        return "date"
+    //    }
+    
+    convenience init(date: String, filepath: String, holiday: String){
+        self.init()
+        self.date = date
+        self.filepath = filepath
+        self.holiday = holiday
+    }
+    
+}
